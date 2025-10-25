5cc8649334b72d67feb928b05509a430773070ad
VIVEK <primevivek14@gmail.com>
2025-10-25 14:51:53 +0530

    hi

--- connect/db.connect.js
+++ connect/db.connect.js
@@ -6,10 +6,10 @@ const mongoURI = process.env.MONGODB;
 const initializeDatabase = async () => {
   try {
     const connection = await mongoose.connect(mongoURI, 
-        {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+        // {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    // }
 );
     if (connection) {
       console.log("connected succesfully");
